81fdf08d9d7a679ab0be679e06bc24807f1f75c2
eweed <35077476+eweed@users.noreply.github.com>
2018-01-08 15:57:19 -0800

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -59,7 +59,7 @@ define(function (require) {
 		payload['arguments'] = payload['arguments'] || {};
 		payload['arguments'].execute = payload['arguments'].execute || {};
 		payload['arguments'].execute.inArguments = [{
-			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"Test__c:choice__c\"}}'
+			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"Test__c:Id\"}}'
 		}];
 
 		payload['metaData'] = payload['metaData'] || {};
